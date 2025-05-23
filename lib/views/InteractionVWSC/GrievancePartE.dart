371aea3d00be1507ff4d7aa5ccb80bfdc327eef4
Shubham Gupta <75482143+shubhamdevgupta@users.noreply.github.com>
2025-05-23 21:41:28 +0530

    imple vwsc part d and part e api calling

--- lib/views/InteractionVWSC/GrievancePartE.dart
+++ lib/views/InteractionVWSC/GrievancePartE.dart
@@ -1,11 +1,15 @@
+import 'package:cno_inspection/provider/vwscInfoProvider/VwscProvider.dart';
 import 'package:cno_inspection/utils/customradiobttn.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 import '../../utils/AppStyles.dart';
 import '../../utils/CommonScreen.dart';
 import '../../utils/CustomRadioQuestion.dart';
+import '../../utils/LoaderUtils.dart';
 import '../../utils/MultiSelectionlist.dart';
+import '../../utils/toast_helper.dart';
 import 'DashboardVWSC.dart';
 
 class GrievancePartE extends StatefulWidget {
@@ -21,171 +25,162 @@ class _GrievancePartE extends State<GrievancePartE> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        // Replace the current route with a new one
-        /*   Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (_) => VillageList()),
-        );*/
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/icons/header_bg.png'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          // Removes the default back button
+          centerTitle: true,
+          title: Text(
+            "Interaction with VWSC",
+            style: AppStyles.appBarTitle,
+          ),
+          leading: IconButton(
 
-        // Return false to prevent the default back navigation behavior
-        return false;
-      },
-      child: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/icons/header_bg.png'), fit: BoxFit.cover),
-        ),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            // Removes the default back button
-            centerTitle: true,
-            title: Text(
-              "Interaction with VWSC",
-              style: AppStyles.appBarTitle,
-            ),
-            leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () {
+              if (Navigator.of(context).canPop()) {
+                Navigator.pop(context);
+              } else {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => Dashboardvwsc()),
+                      (route) => false,
+                );
+              }
+            },
+          ),
 
-              icon: const Icon(Icons.arrow_back, color: Colors.white),
-              onPressed: () {
-                if (Navigator.of(context).canPop()) {
-                  Navigator.pop(context);
-                } else {
-                  Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => Dashboardvwsc()),
-                        (route) => false,
-                  );
-                }
-              },
-            ),
-
-            //elevation
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF096DA8), // Dark blue color
-                    Color(0xFF3C8DBC), // Green color
-                  ],
-                  begin: Alignment.topCenter, // Start at the top center
-                  end: Alignment.bottomCenter, // End at the bottom center
-                ),
+          //elevation
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF096DA8), // Dark blue color
+                  Color(0xFF3C8DBC), // Green color
+                ],
+                begin: Alignment.topCenter, // Start at the top center
+                end: Alignment.bottomCenter, // End at the bottom center
               ),
             ),
-            elevation: 5,
           ),
-          body: Stack(
-            children: [
-              SingleChildScrollView(
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      NewScreenPoints(
-                        no: 5,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Card(
-                          elevation: 5,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(color: Colors.green, width: 1.4),
-                              borderRadius: BorderRadius.circular(14),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black12.withOpacity(0.06),
-                                  blurRadius: 6,
-                                  offset: const Offset(0, 3),
-                                ),
-                              ],
-                            ),
-                            padding: EdgeInsets.all(10),
-                            width: double.infinity,
-                            child:Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                // 1. Grievance redressal mechanism available
-                                Customradiobttn(
-                                  question: "1. Grievance redressal mechanism available:",
-                                  options: ["Yes", "No"],
-                                  selectedOption: selectedGrievanceMechanism,
-                                  onChanged: (val) => setState(() => selectedGrievanceMechanism = val),
-                                ),
+          elevation: 5,
+        ),
+        body: Consumer<Vwscprovider>(
+          builder: (context,vwscProvider,child){
+            return SingleChildScrollView(
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    NewScreenPoints(
+                      no: 5,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        elevation: 5,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.green, width: 1.4),
+                            borderRadius: BorderRadius.circular(14),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12.withOpacity(0.06),
+                                blurRadius: 6,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.all(10),
+                          width: double.infinity,
+                          child:Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // 1. Grievance redressal mechanism available
+                              Customradiobttn(
+                                question: "1. Grievance redressal mechanism available:",
+                                options: vwscProvider.yesNoMap.keys.toList(),
+                                selectedOption: vwscProvider.selectedGrievanceMechanism,
+                                onChanged: (val) => vwscProvider.selectedGrievanceMechanism = val,
+                              ),
+                              SizedBox(height: 16),
 
-                                SizedBox(height: 16),
+                              // 2. How grievances are registered by villagers (multi-select)
+                              CustomMultiSelectChipQuestion(
+                                question: "2. How grievances are registered by the villagers:",
+                                options: vwscProvider.grievanceRegistrationMethodMap.keys.toList(),
+                                selectedValues: vwscProvider.grievanceRegistrationMethods,
+                                onSelectionChanged: (val) {
+                                  vwscProvider.grievanceRegistrationMethods = val;
+                                  print('Selected IDs: ${vwscProvider.selectedGrievanceMethodIds}');
+                                },
+                              ),
 
-                                // 2. How grievances are registered by villagers (multi-select)
-                                CustomMultiSelectChipQuestion(
-                                  question: "2. How grievances are registered by the villagers:",
-                                  options: [
-                                    "Toll free number",
-                                    "Web based portal",
-                                    "Mobile application",
-                                    "Public grievance registration center",
-                                    "Directly calling to PHED/Contractor/O&M agency",
-                                  ],
-                                  selectedValues: grievanceRegistrationMethods,
-                                  onSelectionChanged: (selectedList) {
-                                    setState(() {
-                                      grievanceRegistrationMethods = selectedList;
-                                    });
-                                  },
-                                ),
+                              SizedBox(height: 20),
 
+                              // 3. Turn around time for grievance
 
-                                SizedBox(height: 20),
-
-                                // 3. Turn around time for grievance
-                                Customradiobttn(
-                                  question: "3. Turn around time for grievance:",
-                                  options: ["Same day", "Two days", "Three days", "More than three days"],
-                                  selectedOption: selectedTurnAroundTime,
-                                  onChanged: (val) => setState(() => selectedTurnAroundTime = val),
-                                ),
-                                Align(
-                                  alignment: Alignment.centerRight,
-                                  child: SizedBox(
-                                    height: 35,
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: Colors.green,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(10), // Adjust the radius as needed
-                                        ),
+                              Customradiobttn(
+                                question: "3. Turn around time for grievance:",
+                                options: vwscProvider.turnAroundTimeMap.keys.toList(),
+                                selectedOption: vwscProvider.selectedTurnAroundTime,
+                                onChanged: (val) => vwscProvider.selectedTurnAroundTime = val,
+                              ),
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: SizedBox(
+                                  height: 35,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.green,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10), // Adjust the radius as needed
                                       ),
-                                      onPressed: () {
+                                    ),
+                                    onPressed: () async{
+                                      await LoaderUtils.conditionalLoader(isLoading: vwscProvider.isLoading);
+                                      await vwscProvider.saveGrievanceRedressal(userId: 2232, stateId: 32,
+                                          villageId: 21, grievanceMechanismAvailable: vwscProvider.selectedGrievanceMechanismId,
+                                          grievanceTurnAroundTime: vwscProvider.selectedTurnAroundTimeId, registrationTypes: vwscProvider.selectedGrievanceMethodIds,
+                                          createdBy: 2232);
+                                      if(vwscProvider.status!){
+                                        ToastHelper.showToastMessage( vwscProvider.message!,backgroundColor: Colors.green);
                                         Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => Dashboardvwsc()),);
+                                      }else{
+                                        ToastHelper.showToastMessage( vwscProvider.message!,backgroundColor: Colors.red);
+                                      }
 
-                                      },
-                                      child: Text(
-                                        "SAVE",
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w500,
-                                          color: Colors.white,
-                                        ),
+                                    },
+                                    child: Text(
+                                      "SAVE",
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.white,
                                       ),
                                     ),
                                   ),
                                 ),
-                              ],
-                            )
-                            ,
-                          ),
+                              ),
+                            ],
+                          )
+                          ,
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
+            );
+          },
+        )
       ),
     );
   }
