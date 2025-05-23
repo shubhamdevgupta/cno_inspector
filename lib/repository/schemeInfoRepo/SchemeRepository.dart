5c0ef46ece49ef76672bb68fbecf8f1df3c8ff40
Shubham Gupta <75482143+shubhamdevgupta@users.noreply.github.com>
2025-05-23 12:38:08 +0530

    add repo and provider

---
+++ lib/repository/schemeInfoRepo/SchemeRepository.dart
@@ -0,0 +1,9 @@
+import 'package:cno_inspection/model/DashboardResponse/DashboardResponse.dart';
+
+import '../../services/BaseApiService.dart';
+
+class SchemeRepositoy {
+  final BaseApiService _apiService = BaseApiService();
+
+
+}
---
+++ lib/repository/vwscInfoRepo/VwscRepository.dart
@@ -0,0 +1,9 @@
+import 'package:cno_inspection/model/DashboardResponse/DashboardResponse.dart';
+
+import '../../services/BaseApiService.dart';
+
+class VwscRepository {
+  final BaseApiService _apiService = BaseApiService();
+
+
+}
