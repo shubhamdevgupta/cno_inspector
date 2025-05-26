340ba83cb58f572330253ab34b333db4f19c0520
Shubham Gupta <75482143+shubhamdevgupta@users.noreply.github.com>
2025-05-26 12:12:57 +0530

    add error provider

--- lib/provider/authentication_provider.dart
+++ lib/provider/authentication_provider.dart
@@ -87,7 +87,7 @@ class AuthenticationProvider extends ChangeNotifier {
     try {
       _verifyOtpResponse = await _authRepository.verifyOtp(loginId, otp);
       if (_verifyOtpResponse!.Status == true) {
-        _localStorage.saveInt(AppConstants.prefUserId, 1726);
+        _localStorage.saveInt(AppConstants.prefUserId,verifyOtpResponse!.UserId! );
         onSuccess();
       } else {
         errorMsg = _verifyOtpResponse!.Message!;
